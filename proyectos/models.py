091873649fb9c05ca7db025ff7645b642b63cdd0
ProfL3302 <ProfL3302@lab.aca>
2024-02-08 10:52:41 -0500

    entidad usuario creada con admin

--- proyectos/models.py
+++ proyectos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
